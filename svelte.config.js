4cce1ea9ef6222b66abd2c74f6ec66020ceab709
hrshpalod <harshspalod@gmail.com>
2025-02-13 22:49:42 +0530

    hi

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,16 @@
-import adapter from "@sveltejs/adapter-vercel"
+import adapter from "@sveltejs/adapter-auto"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    inlineStyleThreshold: 150000, // Allow up to 150kb of inlined styles
-    prerender: {
-      handleHttpError: ({ status, path }) => {
-        if (status === 404) {
-          console.warn(`Skipping prerender for: ${path} (404)`)
-          return
-        }
-        throw new Error(`Prerender failed for: ${path}`)
-      },
-    },
+    // allow up to 150kb of style to be inlined with the HTML
+    // Faster FCP (First Contentful Paint) by reducing the number of requests
+    inlineStyleThreshold: 150000,
   },
   preprocess: vitePreprocess(),
 }
